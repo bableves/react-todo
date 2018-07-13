ff68c4e285f8282433e554dec9f9c94cd88ed796
tamaskonyhas <abracadubrah@gmail.com>
2018-07-13 16:00:41 +0200

    Add filter functionality

--- app/api/TodoAPI.jsx
+++ app/api/TodoAPI.jsx
@@ -15,5 +15,30 @@ module.exports = {
     } catch (e) {}
 
     return $.isArray(todos) ? todos : [];
+  },
+  filterTodos: function (todos, showCompleted, searchText) {
+    var filteredTodos = todos;
+
+    // Filter by showCompleted
+    filteredTodos = filteredTodos.filter((todo) => {
+      return !todo.completed || showCompleted;
+    });
+    //Filter by searchText
+    filteredTodos = filteredTodos.filter((todo) => {
+      var text=todo.text.toLowerCase();
+      return searchText.length === 0 || text.indexOf(searchText) > -1;
+    });
+    //Sort todos with non-completed first
+    filteredTodos.sort((a, b) => {
+      if (!a.completed && b.completed) {
+        return -1; // -1 means "a" comes before "b"
+      } else if (a.completed && !b.completed) {
+        return 1; // 1 means "b" comes before "a"
+      } else {
+        return 0; // 0 means there is no need to change order
+      }
+    });
+
+    return filteredTodos;
   }
 };
